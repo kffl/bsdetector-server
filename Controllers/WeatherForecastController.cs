53819161b083209bae2486795dc5dbf338fed3ac
newt0npk <pawel@kuffel.io>
2020-04-02 12:11:28 +0200

    trigger deploy

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace BSDetector.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Raining cats and dogs"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
